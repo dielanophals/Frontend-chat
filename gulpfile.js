5a7947b19858db1c46d9d3d59ab47fb8f815cfed
Dielan <dielano@hotmail.be>
2019-05-24 01:16:43 +0200

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const {src, dest, watch, parallel} = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+function sass2css(done){
+    return src("./sass/style.scss")
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(dest("./css/"))
+        .pipe(browserSync.stream())
+
+    done();
+}
+
+watch('./src/sass/**/*.scss', sass2css)
+
+module.exports.default = parallel(sass2css);
